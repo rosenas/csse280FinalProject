62ac52cb05da29ef7f5df6e5c5857cd063922e75
rosenas <70673175+rosenas@users.noreply.github.com>
2021-10-25 16:47:29 -0400

    tryna set up the server

---
+++ public/app.js
@@ -0,0 +1,41 @@
+const http = require('http');
+
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
+const express = require("express");
+const path = require("path");
+
+const app = express();
+app.use(express.json());
+app.use(express.static("express"));
+
+
+app.get("/", (req, res) => {
+  res.status(200).send();
+});
+
+// app.listen(port, () => {
+//   console.log(`Listening to requests on http://localhost:${port}`);
+// });
+
+app.use('/', function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+const server = http.createServer(app);
+const port = 3000;
+server.listen(port);
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World');
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,10 +1,4 @@
-/**
- * @fileoverview
- * Provides the JavaScript interactions for all pages.
- *
- * @author 
- * PUT_YOUR_NAME_HERE
- */
+
 
 /** namespace. */
 var rhit = rhit || {};
