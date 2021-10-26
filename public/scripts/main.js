93e6e1e8dc1afd4542a0587d640cb262ea5eee31
hawkinwd <hawkinwd@rose-hulman.edu>
2021-10-26 16:44:52 -0400

    more work on login page

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -42,6 +42,23 @@ rhit.main = function () {
 	$(".details").click((event) => {
 		window.location.href = "./details.html"
 	})
+
+	rhit.startFirebaseUI();
 };
+
+rhit.startFirebaseUI = function () {
+	console.log("firebase ui buttons");
+	var uiConfig = {
+		signInSuccessUrl: '/',
+		signInOptions: [
+			firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+			firebase.auth.EmailAuthProvider.PROVIDER_ID,
+			firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+			firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
+		],
+	};
+	const ui = new firebaseui.auth.AuthUI(firebase.auth());
+	ui.start('#firebaseui-auth-container', uiConfig);
+}
 
 rhit.main();
